ca50c943bc95984c7f494d403cb2e0bdc539acb0
brandonguzman12 <66183765+brandonguzman12@users.noreply.github.com>
2022-06-02 20:43:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
-function setup() {
-  // put setup code here
+int x;
+int y;
+int radius;
+int nc;
+void setup(){
+  size(400, 400);
+  background(255, 0, 0);
+  x=40;
+  y=40;
+  radius=40;
+  nc=0;
+  fill(nc);
+  circle(x, y, radius*2);
+  circles();
 }
 
-function draw() {
-  // put drawing code here
+void circles(){
+  while(y<height){
+  while(x<width){  
+    fill(nc);
+    circle(x, y, radius*2);
+    x+=(radius*2);
+    nc+=255/((width/(radius*2))*(height/(radius*2)));
+   }
+  y+=radius*2;
+  x=radius;
 }
+  }
